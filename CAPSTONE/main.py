cb6994bb9e14137949918d2688f78b768f0293b4
macel25 <maricel.lim@cardmri.com>
2025-02-19 16:16:55 +0800

    Add files via upload
    initial commit

---
+++ CAPSTONE/main.py
@@ -0,0 +1,5 @@
+import streamlit as st
+st.title("CAPSTONE PROJECT")
+st.subheader("More project more profit!")
+st.write("Join us now!")
+st.image("PictureEmployee.jpg", "We Provide Good Services")
